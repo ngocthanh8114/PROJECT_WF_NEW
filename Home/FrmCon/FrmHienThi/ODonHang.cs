131278baf4f68cf16808bca1d0d799b8d8a90a2a
Thien Duy <duythien3624@gmail.com>
2024-04-14 09:42:28 +0700

    Them DB DonHang

--- Home/FrmCon/FrmHienThi/ODonHang.cs
+++ Home/FrmCon/FrmHienThi/ODonHang.cs
@@ -17,32 +17,15 @@ namespace Home.FrmCon.FrmHienThi
             InitializeComponent();
         }
 
-        private void ODonHang_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void guna2CustomCheckBox1_Click(object sender, EventArgs e)
-        {
-            DonHang donHang = new DonHang();
-            donHang.loadGiaTien += DonHang_loadGiaTien;
-        }
-
-        private void DonHang_loadGiaTien(object sender, EventArgs e)
-        {
-            
-
-        }
 
         //-------------NgocThanh--------------------
-        public void themDonHang(string tenSP, decimal gia, Image anh)
+        public void themDonHang(string tenSP, decimal gia, int SL, Image anh)
         {
-            //lblMaSP.Text = MaSP;
             lblTenSP.Text = tenSP;
-            lblGia.Text = "Giá: " + gia.ToString() + "VNĐ";
-            //lblNCC.Text = nCC;
-            //lblSL.Text = sL.ToString() + " cái";
-            guna2PictureBox1.Image = anh;
+            lblGia.Text = "Giá: " + gia.ToString() + " VNĐ";
+            numSL.Value = SL;
+            picBoxSP.Image = anh;
+            lblKhuyenMai.Text = gia.ToString() + " VNĐ";
         }
         //-------------NgocThanh--------------------
 
